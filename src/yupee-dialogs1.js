2a42731393034dc9c69570a18f5252a6bf3c266f
AlexandreBrillant <abrillant@japisoft.com>
2026-02-14 16:20:42 +0100

    support for native dialogs

---
+++ src/yupee-dialogs1.js
@@ -0,0 +1,16 @@
+/*
+* Default dialog using the native dialogs for the Yupee framework.
+* Just add this library after the yupee library for usage
+*
+* @author Alexandre Brillant (https://github.com/AlexandreBrillant/)
+* @version 1.0
+*/
+( ( $$ ) => {
+
+    $$.dialogs = {
+        alert: async ( msg ) => new Promise( (resolve, reject) => window.alert( msg ) && resolve( true ) ),
+        confirm: async ( msg ) => new Promise( ( resolve,reject ) => resolve( window.confirm( msg ) ) ),
+        prompt: async ( msg ) => new Promise( ( resolve, reject ) => resolve( window.prompt( msg ) ) )
+    };
+
+} )( $$ );
